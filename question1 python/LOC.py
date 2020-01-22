e4aa07ec01b3a717fd4ad7ce95945bed2df91e76
Ryo Akita <ryo0223@users.noreply.github.com>
2020-01-22 19:21:10 +0000

    Add files via upload
    LOC.py is for question 1 while unittest_tech.py is a test code for the python file.

---
+++ question1 python/LOC.py
@@ -0,0 +1,30 @@
+def LOC_array_gen(message): #generate an array to contain 2nd and 3rd element for each LOC
+    message=message.split("\n")
+    LOC_array=[]
+    for i in message:
+        Edifact=i.split("+")
+        if Edifact[0]=="LOC":
+            LOC_array.append(Edifact[1:3])
+    return LOC_array
+if __name__ == "__main__":
+    given_message="""UNA:+.? '
+    UNB+UNOC:3+2021000969+4441963198+180525:1225+3VAL2MJV6EH9IX+KMSV7HMD+CUSDECU-IE++1++1'
+    UNH+EDIFACT+CUSDEC:D:96B:UN:145050'
+    BGM+ZEM:::EX+09SEE7JPUV5HC06IC6+Z'
+    LOC+17+IT044100'
+    LOC+18+SOL'
+    LOC+35+SE'
+    LOC+36+TZ'
+    LOC+116+SE003033'
+    DTM+9:20090527:102'
+    DTM+268:20090626:102'
+    DTM+182:20090527:102'
+    """
+    
+    answer=LOC_array_gen(given_message)
+    print(answer)
+
+
+
+
+
